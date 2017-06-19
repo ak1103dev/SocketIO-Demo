7efb38dbf2a627e76271ded053d1ee4427f1616c
ak1103dev <ak.apichan1103@gmail.com>
2017-06-19 09:33:12 +0700

    :sparkles: disconnect socket

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
-io.on('connection', (socket) => console.log('a user connected'));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
 
 http.listen(port, () => console.log(`listening on port ${port}`));
 
