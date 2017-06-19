cff37b692401d43dfe6b2da84d4f9c765bc10e90
ak1103dev <ak.apichan1103@gmail.com>
2017-06-19 09:28:55 +0700

    :sparkles: connect socket

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const port = 1234;
 const app = require('express')();
-const port = 3000
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+io.on('connection', (socket) => console.log('a user connected'));
+
+http.listen(port, () => console.log(`listening on port ${port}`));
 
