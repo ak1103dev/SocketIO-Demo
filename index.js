11bf14a76a0f2691417fef6f74d00716563175bc
ak1103dev <ak.apichan1103@gmail.com>
2017-06-19 09:07:56 +0700

    :lipstick: chat UI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const port = 3000
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
