87c9350adaab9ceaeaf6556204e3771cdb9a1a44
ak1103dev <ak.apichan1103@gmail.com>
2017-06-20 09:51:56 +0700

    :sparkles: connected status
    Broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const io = require('socket.io')(http);
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => console.log('user disconnected'));
+  io.emit('chat message', '<a user connected>');
+  // console.log('a user connected');
+  socket.on('disconnect', () => io.emit('chat message', '<user disconnected>'));
+  // socket.on('disconnect', () => console.log('user disconnected'));
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
